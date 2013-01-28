fdaf55c20cfb9d28db7f47384bca14af35f9dfc7
ivolapumapm87 <agil3277@caelum-Satellite-L655>
2013-01-28 21:47:23 -0200

    Inclusao public class Main no arquivo...

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
-public static void main(String[] args) {
-	System.out.println("Exemplo de teste de uso do git");
-	System.out.println("Teste de commit no git")
+public Main {
+	public static void main(String[] args) {
+		System.out.println("Exemplo de teste de uso do git");
+		System.out.println("Teste de commit no git")
+	}
 }
