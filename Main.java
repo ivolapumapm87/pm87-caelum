d6ea1007c12b160c8ec1efda6e73ebef05bc606f
ivolapumapm87 <ivolapumaspam@gmail.com>
2013-01-28 22:29:24 -0200

    Outro commit no git...

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Exemplo de teste de uso do git");
 		System.out.println("Teste de commit no git");
-		System.out.println("Aula de git.");
+		System.out.println("Aula de github.");
 	}
 }
